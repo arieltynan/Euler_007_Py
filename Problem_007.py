13de39515e986313266d6c4ec28ec09ea9a62f2b
Ariel Tynan <90798242+arieltynan@users.noreply.github.com>
2022-01-23 15:07:52 -0700

    Add files via upload

---
+++ Problem_007.py
@@ -0,0 +1,19 @@
+# 10001st prime
+# Using Sieve of Eratosthenes
+MAX_PRIME = 2000000
+A = [True]*(MAX_PRIME+1)
+count = 0
+for i in range(2, int(MAX_PRIME**(1/2)+1)):
+    if A[i]:
+        j = i**2
+        #print(i)
+        while j <= MAX_PRIME:
+            A[j] = False
+            j += i
+for i in range(2, MAX_PRIME):
+    if A[i]:
+        count = count + 1
+        if count == 10001:
+            print(i)
+            break
+
